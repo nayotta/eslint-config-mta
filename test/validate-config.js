30dda42e965dc92c025404cc04766f67c0967771
classlfz <classlfz@qq.com>
2021-08-12 18:39:42 +0800

    feat: initialize eslint style config

---
+++ test/validate-config.js
@@ -0,0 +1,16 @@
+const eslint = require('eslint')
+const test = require('tape')
+
+test('load config in eslint to validate all rule syntax is correct', function (t) {
+	const CLIEngine = eslint.CLIEngine
+
+	const cli = new CLIEngine({
+		useEslintrc: false,
+		configFile: 'eslintrc.json'
+	})
+
+	const code = 'const foo = 1\nconst bar = function () {}\nbar(foo)\n'
+
+	t.equal(cli.executeOnText(code).errorCount, 0)
+	t.end()
+})
